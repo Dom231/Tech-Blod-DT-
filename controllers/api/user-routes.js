bcaa7dc344705a798e07365d18523523188c712a
Dom231 <114959233+Dom231@users.noreply.github.com>
2023-01-30 16:26:21 -0600

    basic views templete

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -10,7 +10,7 @@ const {User} = require('../../models');
         const user = userData.get({ plain: true });
         console.log(user);
         // Then, the 'dish' template is rendered and dish is passed into the template.
-        res.render('Dashboard', user);
+        res.render('dashboard', user);
         } catch (err) {
             res.status(500).json(err);
         }
