afac976e58398c7cd6d210ffc50eca1e95ac7e56
Dom231 <114959233+Dom231@users.noreply.github.com>
2023-01-30 16:13:14 -0600

    routes

---
+++ controllers/api/post-routues.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const {Post} = require('../../models');
+
+ router.get('/:id', async (req,res)=>{
+    try {
+        // Search the database for a dish with an id that matches params
+        const postData = await Post.findByPk(req.params.id);
+        console.log(postData)
+        // We use .get({ plain: true }) on the object to serialize it so that it only includes the data that we need. 
+        const post = postData.get({ plain: true });
+        console.log(post);
+        // Then, the 'dish' template is rendered and dish is passed into the template.
+        res.render('singlepost', post);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+ })
+
+ router.get('/', async (req,res)=>{
+    try {
+        // Search the database for a dish with an id that matches params
+        const postData = await Post.findAll();
+        console.log(postData)
+        // We use .get({ plain: true }) on the object to serialize it so that it only includes the data that we need. 
+        const post = postData.get({ plain: true });
+        console.log(post);
+        // Then, the 'dish' template is rendered and dish is passed into the template.
+        res.render('allpost', post);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+ })
+
+ router.post('/', async (req,res)=>{
+    try {
+        const TData = await Category.create(req.body);
+        res.status(200).json(TData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
+ })
+
+
+ module.exports = router;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {User} = require('../../models');
+
+ router.get('/:id', async (req,res)=>{
+    try {
+        // Search the database for a dish with an id that matches params
+        const userData = await User.findByPk(req.params.id);
+        console.log(userData)
+        // We use .get({ plain: true }) on the object to serialize it so that it only includes the data that we need. 
+        const user = userData.get({ plain: true });
+        console.log(user);
+        // Then, the 'dish' template is rendered and dish is passed into the template.
+        res.render('Dashboard', user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+ })
+
+ module.exports = router;
